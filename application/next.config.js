b35535d48bbd04f7566d56f5598f58cd36827480
skyglass <skyglass2001@gmail.com>
2024-05-18 15:45:48 +0200

    improved environment variables config

--- application/next.config.js
+++ application/next.config.js
@@ -1,5 +1,4 @@
 /** @type {import('next').NextConfig} */
-const config = require("./config");
 
 /**
  * nextjs typescript stripe webhook example
@@ -19,25 +18,6 @@ const nextConfig = {
         hostname: "res.cloudinary.com",
       },
     ],
-  },
-  env: {
-    DB_URI: config.DB_URI,
-    API: config.API,
-    NEXTAUTH_SECRET: config.NEXTAUTH_SECRET,
-    GOOGLE_CLIENT_ID: config.GOOGLE_CLIENT_ID,
-    GOOGLE_CLIENT_SECRET: config.GOOGLE_CLIENT_SECRET,
-    CLOUDINARY_CLOUD_NAME: config.CLOUDINARY_CLOUD_NAME,
-    CLOUDINARY_API_KEY: config.CLOUDINARY_API_KEY,
-    CLOUDINARY_API_SECRET: config.CLOUDINARY_API_SECRET,
-    STRIPE_API_KEY: config.STRIPE_API_KEY,
-    STRIPE_SECRET_KEY: config.STRIPE_SECRET_KEY,
-    DOMAIN: config.DOMAIN,
-    NEXTAUTH_URL: config.NEXTAUTH_URL,
-    STRIPE_WEBHOOK_SECRET: config.STRIPE_WEBHOOK_SECRET,
-    STRIPE_TAX_RATE: config.STRIPE_TAX_RATE,
-    STRIPE_SHIPPING_RATE: config.STRIPE_SHIPPING_RATE,
-    GMAIL_AUTH_USER: config.GMAIL_AUTH_USER,
-    GMAIL_AUTH_PASS: config.GMAIL_AUTH_PASS,
   },
 };
 
